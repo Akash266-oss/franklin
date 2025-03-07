1140e0eebaa3a1ef981c872029ead3b3233e5834
Akash266-oss <archanagowri247@gmail.com>
2025-03-07 11:54:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,26 +1,2 @@
 #!/bin/bash
-
-# Variables
-IMAGE_NAME="akash3020/my-app"
-TAG="latest"  # Change this to match your versioning strategy
-
-echo "Building Docker image: $IMAGE_NAME:$TAG"
-
-# Build Docker image
-docker build -t $IMAGE_NAME:$TAG .
-
-if [ $? -ne 0 ]; then
-    echo "Docker build failed!"
-    exit 1
-fi
-
-echo "Pushing image to Docker Hub..."
-docker push $IMAGE_NAME:$TAG
-
-if [ $? -ne 0 ]; then
-    echo "Docker push failed!"
-    exit 1
-fi
-
-echo "Build and push completed successfully!"
-
+docker build -t test .
